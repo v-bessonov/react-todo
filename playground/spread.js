232060a36580986f383c1c10fd5e62641d315da3
vladdnc <vladdnc@mail.ru>
2017-06-08 13:53:01 +0300

    Spead operator

---
+++ playground/spread.js
@@ -0,0 +1,38 @@
+// function add(a,b){
+//   return a+b;
+// }
+//
+// console.log(add(3,1));
+//
+// var toAdd = [9,5];
+//
+// console.log(add(...toAdd));
+
+
+// var groupA = ['A','B'];
+// var groupB = ['C','D', 'E'];
+//
+//
+// var final = [...groupB, 3, ...groupA];
+//
+// console.log(final);
+
+var person = ['Vlad', 33];
+var personTwo = ['Jen', 29];
+
+
+var names= ['Mike', 'Ben'];
+
+
+function greet (name, age){
+  console.log('Hi ' + name + ', you are ' + age);
+}
+
+greet(...person);
+greet(...personTwo);
+
+var final = ['Vlad', ...names];
+
+final.forEach(function(name){
+  console.log('Hi ' + name);
+});
