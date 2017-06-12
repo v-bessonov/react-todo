fa3cfff86df2cfbd99900399dad81819d1a3ac0f
vladdnc <vladdnc@mail.ru>
2017-06-12 22:08:15 +0300

    Add redux store

---
+++ app/store/configureStore.jsx
@@ -0,0 +1,9 @@
+var redux = require('redux');
+var {searchTextReducer, showCompletedReducer, todosReducer} = require('reducers');
+
+export var configure = () => {
+  var reducer = redux.combineReducers({searchText: searchTextReducer, showCompleted: showCompletedReducer, todos: todosReducer});
+
+  var store = redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  return store;
+}
